9a0983eeccae0ee2f9bd85107d5f99845b500378
sarthak <sarthakssingh49@gmail.com>
2026-02-01 01:25:19 +0530

    Setup router and layout structure

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,59 +1,20 @@
-import { useEffect, useRef, useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-import './App.css'
+//import './App.css'
+import { Outlet } from 'react-router-dom'
+import About from './pages/About'
+import Home from './pages/Home'
+import Navbar from './components/Navbar'
 
 function App() {
-  
-  const [text, setText] = useState("");
-  const started = useRef(false);
-  const finished = useRef(false);
-  const toShow = useRef(false);
-  const wpm = useRef<number>(0);
-  const [_, setDone] = useState(false);
-  const textRef = useRef("");
-
-  const startTimer = () => {
-    if(started.current) return;
-    started.current = true;
-
-    setTimeout(() => {
-      finished.current = true;
-      toShow.current = true;
-      const countWords = (text: string) => {
-        return text.trim().split(/\s+/).length;
-      };
-      wpm.current = countWords(textRef.current)/10;
-      setDone(true);
-
-    }, 10000)
-  }
-
-  const handleOnChange = (e: any) => {
-    if(finished.current) return;
-    startTimer();
-    setText(e.target.value);
-    textRef.current =  e.target.value;
-
-  }
-    
-    
 
   return (
     <>
-      {/* <input type = "text" value={val} onChange={(e)=>setVal(e.target.value)} ></input> */}
-      <div>
-        <h1>Start the timer of 60 secs</h1>
-        
-        <br />
-        <textarea onChange = {handleOnChange} ></textarea>
-        <br />
-        {text}
-        <br />
-        {toShow.current && wpm.current.toString()}
-      </div>
-      
+    <Navbar />
+    
+    <Outlet />
+  
     </>
+
+
   )
 }
 
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import "./index.css";
+import App from "./App";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,   
+    children: [
+      { index: true, element: <Home /> },   
+      { path: "about", element: <About /> } 
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);

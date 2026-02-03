94268e278cf12a76f50ee14851517b62c18f6a88
sarthak <sarthakssingh49@gmail.com>
2026-02-03 23:59:19 +0530

    controllers for auth

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -1,7 +1,29 @@
-export const loginController = () => {
+import { Request, Response, NextFunction } from "express";
+import { loginService, registerService } from "../services/auth.service";
 
+export const loginController = async (req: Request, res: Response, next: NextFunction) => {
+    
+    try{
+        const {username, password} = req.body;
+        
+        const result = await loginService({username, password});
+       const token = result.token;
+       const user = result.user;
+
+       return res.status(200).json({token, user});
+    }
+    catch(err){
+        console.log(err);
+    }
 }
 
-export const registerController = () => {
-
+export const registerController = async (req: Request, res: Response, next: NextFunction) => {
+    try{
+        const {username, password} = req.body;
+        const result = await registerService({username, password});
+        return res.status(201).json(result);
+    }
+    catch(err){
+        console.log(err);
+    }
 }
---
+++ server/src/services/auth.service.ts
@@ -0,0 +1,10 @@
+
+
+export const loginService = async ({}) => {
+   
+    const token = null;
+    const user = null;
+    return {token, user}
+}
+
+export const registerService = ({}) => {}
